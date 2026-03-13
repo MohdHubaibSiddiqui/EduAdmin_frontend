d5c3c7e9bccce930d3da7260596ac9f9e6979011
Hubaib Siddiqui <hubaibsiddiqui123@gmail.com>
2026-03-14 04:10:12 +0530

    try backend to connected

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   // baseURL: "http://localhost:5000/api",
+// baseURL: "https://eduadmin-backend-laqn.onrender.com/api",
+// });
 const API = axios.create({
-  // baseURL: "http://localhost:5000/api",
-baseURL: "https://eduadmin-backend-laqn.onrender.com/api",
+  baseURL: "https://eduadmin-backend-laqn.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
